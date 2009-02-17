d532a84604df7662a6a3677aa937d218966cb186
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2009-02-17 22:24:47 +0000

    Removed old structure

--- trunk/_docs/com_evechartracking.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `jos_components` WHERE `option`='com_evechartracking';
-
-INSERT INTO `jos_components` (`name`, `link`, `menuid`, `parent`, `admin_menu_link`, `admin_menu_alt`, `option`, `ordering`, `admin_menu_img`, `iscore`, `params`, `enabled`) VALUES
-('EVE Character Tracking', 'option=com_evechartracking', 0, 0, 'option=com_evechartracking&view=dbcheck', 'EVE Character Tracking', 'com_evechartracking', 0, 'components/com_eve/assets/icon-char-16.png', 0, '', 1);
--- trunk/administrator/components/com_eve/sql/uninstall.compat.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS `#__eve_characters`;
-DROP TABLE IF EXISTS `#__eve_corporations`;
-DROP TABLE IF EXISTS `#__eve_alliances`;
