756274cf387926f1270f408d8cf08d3f51abb99e
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2009-02-20 00:29:59 +0000

    Updated character sheet component

--- _docs/plugins.sql
+++ _docs/plugins.sql
@@ -1,4 +1,4 @@
 INSERT INTO `jos_plugins` (`name`, `element`, `folder`, `access`, `ordering`, `published`, `iscore`, `client_id`, `checked_out`, `checked_out_time`, `params`) VALUES
-('User - EVE', 'eve', 'user', 0, 0, 1, 0, 0, 0, '2008-04-09 00:00:00', ''),
-('EVE API - EVE Online', 'eve', 'eveapi', 0, 0, 1, 0, 0, 0, '2008-04-09 00:00:00', ''),
+('User - EVE', 'eve', 'user', 0, 0, 1, 0, 0, 0, '0000-00-00 00:00:00', ''),
+('EVE API - EVE Online', 'calls', 'eveapi', 0, 0, 1, 0, 0, 0, '0000-00-00 00:00:00', ''),
 ('System - EVE', 'eve', 'system', 0, 0, 1, 0, 0, 0, '0000-00-00 00:00:00', 'igb_template=eve_igb\ntrustme=Add this site to your trusted sites to allow advanced features like autologin. <br /><br />More to come.<br /><br />Warning: this is test site, using alt char is recommended.\n');
--- com_evecharsheet/site/views/sheet/tmpl/default.php
+++ com_evecharsheet/site/views/sheet/tmpl/default.php
@@ -11,13 +11,38 @@ defined('_JEXEC') or die();
 	<?php echo $this->escape($this->params->get('page_title')); ?>
 </div>
 <?php endif; ?>
+
 <div>
+	<img src="http://img.eve.is/serv.asp?s=256&c=<?php echo $this->character->characterID; ?>" /> <br />
+	<?php echo JText::_('Character Name'); ?>: <?php echo $this->character->name; ?> <br />
+	<?php echo JText::_('Race'); ?>: <?php echo $this->character->race; ?> <br />
+	<?php echo JText::_('Gender'); ?>: <?php echo $this->character->gender; ?> <br />
+	<?php echo JText::_('Blood Line'); ?>: <?php echo $this->character->bloodLine; ?> <br />
+	<?php echo JText::_('Ballance'); ?>: <?php echo number_format($this->character->balance); ?> <br />
+	<?php echo JText::_('Corporation'); ?>: <?php echo $this->corporation->corporationName; ?>
+		 [<?php echo $this->corporation->ticker; ?>] <br />
+</div>
+
 <div>
 <?php foreach ($this->groups as $group): ?>
-	<tr>
-		<td colspan="2">
-			<?php echo JText::_($group->groupName); ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-<div>
+	<h3>
+	<?php echo $group->groupName; ?>
+	</h3>
+	<table class="skill-group">
+	<?php foreach ($group->skills as $skill): ?>
+		<tr>
+			<td class="skill-label">
+				<?php echo $skill->typeName; ?>
+			</td>
+			<td class="skill-level">
+				<img src="<?php echo JURI::base(); ?>components/com_evecharsheet/assets/level<?php echo $skill->level; ?>.gif" border="0" alt="Level <?php echo $skill->level; ?>" title="<?php echo number_format($skill->skillpoints); ?>" />
+			</td>
+		</tr>
+	<?php endforeach; ?>
+	</table>
+	<div>
+		<?php echo JText::sprintf('%s skills trained for total of %s skillpoints', $group->skillCount, number_format($group->skillpoints)); ?><br />
+		<?php echo JText::sprintf('Skill Cost %s', number_format($group->skillPrice)); ?>
+	</div>
+	<?php endforeach; ?>
+</div>
