45bd4ffc6aae435b5a7b0e164ba276d7d37cf052
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-02-20 19:54:25 +0000

    Fixed installers

--- com_evecharsheet/admin/sql/install.sql
+++ com_evecharsheet/admin/sql/install.sql
@@ -106,8 +106,8 @@ PRIMARY KEY ( `characterID` )
 INSERT INTO `#__eve_apicalls` (`type`, `call`, `authentication`, `authorization`, `delay`, `params`) VALUES 
 ('char', 'SkillQueue', 'Character', 'Limited', 0, '');
 
-INSERT INTO `#__eve_sections` ( `name` , `title` , `alias` , `entity` , `component` , `view` , `layout` , `ordering` , `published` ) VALUES 
-('charsheet', 'Character Sheet', 'character-sheet', 'character', 'charsheet', 'character', null, '0', '1');
+INSERT INTO `#__eve_sections` ( `name` , `title` , `alias` , `entity` , `component` , `view` , `layout` , `ordering` , `published`, `access` ) VALUES 
+('charsheet', 'Character Sheet', 'character-sheet', 'character', 'charsheet', 'character', '', '0', '1', '1');
 
 INSERT INTO #__eve_roles (`roleID`, `roleName`) VALUES
 (1, 'roleDirector'),
