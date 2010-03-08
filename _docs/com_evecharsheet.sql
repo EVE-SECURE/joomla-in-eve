c7b8ee87f504d99408806ea04625912c77c229e0
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-03-08 21:23:50 +0000

    cleareing jos_eve_sections in _doc/*.sql quick install scripts from issue #10

--- _docs/com_evecharsheet.sql
+++ _docs/com_evecharsheet.sql
@@ -79,6 +79,7 @@ DELETE FROM `jos_components` WHERE `option`='com_evecharsheet';
 INSERT INTO `jos_components` (`name`, `link`, `menuid`, `parent`, `admin_menu_link`, `admin_menu_alt`, `option`, `ordering`, `admin_menu_img`, `iscore`, `params`, `enabled`) VALUES
 ('EVE Character Sheet', 'option=com_evecharsheet', 0, 0, 'option=com_evecharsheet', 'EVE Character Sheet', 'com_evecharsheet', 0, 'components/com_eve/assets/icon-char-16.png', 0, '', 1);
 
+DELETE FROM `jos_eve_sections` WHERE `component`='charsheet';
 INSERT INTO `jos_eve_sections` ( `name` , `title` , `alias` , `entity` , `component` , `view` , `layout` , `ordering` , `published`, `access` ) VALUES 
 ('charsheet', 'Character Sheet', 'character-sheet', 'character', 'charsheet', 'character', '', '0', '1', '1');
 
