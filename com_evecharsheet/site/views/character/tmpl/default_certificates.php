c67237e99db989d1f8198a94dbb0796c7b95e273
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-03-11 10:46:09 +0000

    collapsible skills amd certificates for character sheets

--- com_evecharsheet/site/views/character/tmpl/default_certificates.php
+++ com_evecharsheet/site/views/character/tmpl/default_certificates.php
@@ -5,24 +5,20 @@
  
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die();
-
-jimport('joomla.html.pane');
-
-$this->something = $this->params->get('showcertificates') == 2;
-$pane = JPane::getInstance('sliders', array('allowAllClose' => 'true'));
-
 ?>
+
 <div class="evecharsheet-certificates">
 <h3><?php echo JText::_('Certificates'); ?></h3>
-<?php if ($this->something): ?>
-	<?php echo $pane->startPane("certificate-pane"); ?>
-<?php endif; ?>
+<a class="expand-all-certificates" href="#" title="<?php echo JText::_('Expand all certificates'); ?>">
+	<?php echo JText::_('Expand all certificates'); ?>
+</a> 
+| 
+<a class="collapse-all-certificates" href="#" title="<?php echo JText::_('Collapse all certificates'); ?>">
+	<?php echo JText::_('Collapse all certificates'); ?>
+</a>
+
 <?php foreach ($this->categories as $category): ?>
-	<?php if ($this->something): ?>
-		<?php echo $pane->startPanel($this->escape($category->categoryName), "certificate-category-".$category->categoryID); ?>
-	<?php else: ?>
 	<h4><?php echo $this->escape($category->categoryName); ?></h4>
-	<?php endif; ?>
 	<?php if ($category->certificates): ?>
 		<table class="certificate-category">
 		<?php foreach ($category->certificates as $certificate): ?>
@@ -36,14 +32,13 @@ $pane = JPane::getInstance('sliders', array('allowAllClose' => 'true'));
 			</tr>
 		<?php endforeach; ?>
 		</table>
+		<div>
+			<?php echo JText::sprintf('%s certificates in this category', count($category->certificates)); ?>
+		</div>
 	<?php else: ?>
-		<?php echo JText::_('No certificates in this category'); ?>
-	<?php endif; ?>
-	<?php if ($this->something): ?>
-		<?php echo $pane->endPanel(); ?>
+		<div class="certificate-category">
+		</div>
+			<?php echo JText::_('No certificates in this category'); ?>
 	<?php endif; ?>
 <?php endforeach; ?>
-<?php if ($this->something): ?>
-	<?php echo $pane->endPane(); ?>
-<?php endif; ?>
 </div>
