7958c09b6c0f4234abc21630050a0236dcd161f8
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-05-14 17:00:39 +0000

    asset list front end files and models

---
+++ com_eveassetlist/site/view.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @version		$Id$
+ * @author		Pavol Kovalik
+ * @package		Joomla! in EVE
+ * @subpackage	Asset List
+ * @copyright	Copyright (C) 2008 Pavol Kovalik. All rights reserved.
+ * @license		GNU/GPL, see http://www.gnu.org/licenses/gpl.html
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+jimport( 'joomla.application.component.view');
+
+abstract class EveassetlistView extends JView 
+{
+	public $params;
+	public $state;
+	public $items;
+	public $pagination;
+
+	function display($tpl = null) {
+		$app = JFactory::getApplication();
+		
+		$state		= $this->get('State');
+		$params		= $this->get('Params');
+		$item		= $this->get('Item');
+		$listState	= $this->get('State', 'list');
+		$items		= $this->get('Items', 'list');
+		$pagination	= $this->get('Pagination', 'list');
+		
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+		if (count($errors = $this->get('Errors', 'list'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+		
+		$this->_setEntity($item, $params);
+		
+		$refTypeOption = JHTML::_('select.option', '-1', JText::_('Com_Eveassetlist_REF_TYPE_0_OPTION'), 'refTypeID', 'refTypeName');
+		array_unshift($refTypes, $refTypeOption);
+
+		$this->assignRef('params', 		$params);
+		$this->assignRef('state',		$state);
+		$this->assignRef('items',		$items);
+		$this->assignRef('listState',	$listState);
+		$this->assignRef('pagination',	$pagination);
+		
+		parent::display();
+		$this->_setPathway();
+	}
+	
+	protected function _setEntity($item, $params) 
+	{
+		
+	}
+
+	protected function _setPathway()
+	{
+		$menus = &JSite::getMenu();
+		$menu  = $menus->getActive();
+		if (!$menu || $menu->component == 'com_eveassetlist') {
+			return;
+		}
+		
+		$app = JFactory::getApplication();
+		$pathway = $app->getPathway();
+		
+		$view = JArrayHelper::getValue($menu->query, 'view');
+		switch ($view) {
+			case null:
+				$pathway->addItem($this->character->allianceName, 
+					EveRoute::_('alliance', $this->character));
+			case 'alliance':
+				$pathway->addItem($this->character->corporationName, 
+					EveRoute::_('corporation', $this->character, $this->character));
+			case 'corporation':
+			case 'user':
+				$pathway->addItem($this->character->name, 
+					EveRoute::_('character', $this->character, $this->character, $this->character));
+			case 'character':
+				$pathway->addItem(JText::_('Asset List'), 
+					EveRoute::_('charassetlist', $this->character, $this->character, $this->character));
+		}
+	}
+	
+}
