a1423d82a2d5e25adfdd65c78f4948fa87d90907
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-06-11 15:06:41 +0000

    prepare to set properties

--- com_eve/plugins/system_eve/eve/component/modellist.php
+++ com_eve/plugins/system_eve/eve/component/modellist.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version		$Id: modellist.php 11952 2009-06-01 03:21:19Z robs $
+ * @version		$Id$
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
  * @copyright	Copyright (C) 2008 - 2009 JXtended, LLC. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
