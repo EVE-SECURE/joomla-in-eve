568301903cd5b098bd0711a89493e5b26ba931bb
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-06-16 15:38:22 +0000

    com_eveindustryjons

---
+++ com_eveindustryjobs/site/views/corporation/tmpl/default.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @version		$Id$
+ */
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+JHTML::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eve'.DS.'helpers'.DS.'html');
+JHTML::_('behavior.mootools');
+JHTML::_('eve.contextmenu');
+JHTML::stylesheet('component.css', 'media/com_eveindustryjobs/css/');
+?>
+
+<div class="com-eveindustryjobs<?php echo $this->params->get('pageclass_sfx'); ?>">
+<?php if ($this->params->get('show_page_title', 1)) : ?>
+	<h1><?php echo $this->escape($this->params->get('page_title')); ?></h1>
+<?php endif; ?>
+
+<form action="<?php echo EveRoute::_('corpindustryjobs', $this->corporation, $this->corporation); ?>" name="adminForm" method="post">
+<div class="filter">
+	<?php echo JHTML::_('filter.search', $this->listState->get('filter.search')); ?>
+</div>
+<?php echo $this->loadTemplate('list'); ?>
+
+<?php echo $this->pagination->getListFooter(); ?>
+<input type="hidden" name="filter_order" value="<?php echo $this->listState->get('list.ordering', 'al.itemID'); ?>" />
+<input type="hidden" name="filter_order_Dir" value="<?php echo $this->listState->get('list.direction', 'asc'); ?>" />
+</form>
+</div>
