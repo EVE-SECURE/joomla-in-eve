89e0bc3a89362e1fde3a4da28cd6ceff43d60ea4
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-06-23 17:53:05 +0000

    com_evemarketorders - added style sheet

--- com_evemarketorders/site/views/_common/tmpl/default_list.php
+++ com_evemarketorders/site/views/_common/tmpl/default_list.php
@@ -9,19 +9,19 @@ defined('_JEXEC') or die();
 <table class="list">
 	<thead>
 		<tr>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Order_ID', 'mo.orderID', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Order_ID', 'mo.orderID', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
 			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Item_Type', 'inv.typeName', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Volume_Entered', 'mo.volEntered', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Volume_Entered', 'mo.volEntered', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Volume_Remaining', 'mo.volRemaining', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Volume_Remaining', 'mo.volRemaining', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Volume_Min', 'mo.minVolume', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Volume_Min', 'mo.minVolume', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Price', 'mo.price', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Price', 'mo.price', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Escrow', 'mo.escrow', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Escrow', 'mo.escrow', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
 			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Order_State', 'mo.orderState', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
@@ -29,7 +29,7 @@ defined('_JEXEC') or die();
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
 			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Station_Name', 'sta.stationName', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
-			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Range', 'mo.range', 
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Range', 'mo.range', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
 			<th><?php echo JHTML::_('grid.sort',  'Com_Evemarketorders_Duration', 'mo.duration', 
 					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
@@ -44,19 +44,19 @@ defined('_JEXEC') or die();
 	<tbody>
 	<?php foreach ($this->items as $item): ?>
 		<tr>
-			<td><?php echo $item->orderID; ?></td>
+			<td class="number"><?php echo $item->orderID; ?></td>
 			<td><?php echo JHTML::_('evelink.type', $item); ?>
-			<td><?php echo number_format($item->volEntered, 0); ?>
-			<td><?php echo number_format($item->volRemaining, 0); ?>
-			<td><?php echo number_format($item->minVolume, 0); ?>
-			<td><?php echo number_format($item->price, 2); ?>
-			<td><?php echo number_format($item->escrow, 2); ?>
+			<td class="number"><?php echo number_format($item->volEntered, 0); ?>
+			<td class="number"><?php echo number_format($item->volRemaining, 0); ?>
+			<td class="number"><?php echo number_format($item->minVolume, 0); ?>
+			<td class="number"><?php echo number_format($item->price, 2); ?>
+			<td class="number"><?php echo number_format($item->escrow, 2); ?>
 			<td><?php echo $this->orderStateName($item->orderState); ?>
 			<td><?php echo $this->bidName($item->bid); ?>
 			<td><?php echo JHTML::_('evelink.station', $item); ?>
 			<td><?php echo $this->rangeName($item->range); ?>
-			<td><?php echo $item->duration; ?>
-			<td><?php echo JHTML::date($item->issued); ?></td>
+			<td class="number"><?php echo $item->duration; ?>
+			<td><?php echo JHTML::date($item->issued, JText::_('Com_Evemarketorders_Format_Datetime_List')); ?></td>
 
 			<?php if ($this->state->get('entity') == 'user'): 
 				$character = $this->characters[$item->entityID];
