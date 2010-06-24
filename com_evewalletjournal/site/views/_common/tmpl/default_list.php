67e986cdc4341b22c662e47ed9998605b26bbf95
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-06-24 21:10:59 +0000

    com_evewalletjournal - added style sheet and moved table to _common

---
+++ com_evewalletjournal/site/views/_common/tmpl/default_list.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @version		$Id$
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+?>
+<table class="list">
+	<thead>
+		<tr>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Ref_ID', 'wj.refID', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Ref_Type', 'rt.refTypeName', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Date', 'wj.date', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Owner1', 'wj.ownerName1', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Owner2', 'wj.ownerName2', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Argument', 'wj.argName1', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Amount', 'wj.amount', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th class="number"><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Ballance', 'wj.balance', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<th><?php echo JHTML::_('grid.sort',  'Com_Evewalletjournal_Reason', 'wj.reason', 
+					$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+					
+			<?php if ($this->state->get('entity') == 'user'): ?>
+				<th><?php echo JHTML::_('grid.sort',  'Com_Everesearch_Character', 'characterName', 
+						$this->listState->get('list.direction'), $this->listState->get('list.ordering')); ?></th>
+			<?php endif; ?>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($this->items as $item): ?>
+		<tr>
+			<td><?php echo $item->refID; ?></td>
+			<td><?php echo $this->escape($item->refTypeName); ?></td>
+			<td><?php echo JHTML::date($item->date); ?></td>
+			<td><?php echo $this->escape($item->ownerName1); ?></td>
+			<td><?php echo $this->escape($item->ownerName2); ?></td>
+			<td><?php echo $this->getArgument($item); ?></td>
+			<td class="number"><?php echo number_format($item->amount, 2); ?></td>
+			<td class="number"><?php echo number_format($item->balance, 2); ?></td>
+			<td><?php echo $this->getReason($item); ?></td>
+
+			<?php if ($this->state->get('entity') == 'user'): 
+				$character = $this->characters[$item->characterID];
+				?>
+				<td>
+					<a href="<?php echo EveRoute::_('charresearch', $character, $character, $character); ?>">
+						<?php echo $this->escape($item->characterName); ?>
+					</a>
+				</td>
+			<?php endif; ?>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
