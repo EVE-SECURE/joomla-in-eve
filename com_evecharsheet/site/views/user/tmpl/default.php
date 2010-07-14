ffc80964ebb1743c27067c36488fbc6cf064f651
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-07-14 17:13:40 +0000

    com_evecharsheet - added user view (list of characters)

---
+++ com_evecharsheet/site/views/user/tmpl/default.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version		$Id$
+ */
+ 
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die();
+
+JHTML::addIncludePath(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_eve'.DS.'helpers'.DS.'html');
+JHTML::stylesheet('component.css', 'media/com_evecharsheet/css/');
+JHTML::_('eve.contextmenu');
+$pageClass = $this->params->get('pageclass_sfx');
+?>
+
+<div class="com-evecharsheet<?php echo $pageClass ? ' '.$pageClass : ''; ?>">
+<?php if ($this->params->get('show_page_title', 1)) : ?>
+	<h1><?php echo $this->escape($this->params->get('page_title')); ?></h1>
+<?php endif; ?>
+
+<div class="evecharsheet-list">
+	<?php foreach ($this->characters as $character) : ?>
+		<div>
+			<a href="<?php echo EveRoute::_('charsheet', $character, $character, $character); ?>">
+				<?php echo JHTML::_('image', 'http://img.eve.is/serv.asp?s=64&c='.$character->characterID, $character->name); ?>
+			</a>
+			<br />
+			<p>
+			<a href="<?php echo EveRoute::_('charsheet', $character, $character, $character); ?>">
+				<?php echo $this->escape($character->name);?>
+			</a>
+			</p>
+		</div>
+	<?php endforeach; ?>
+</div>
+</div>
