2dd980d3a2ac253233cd40a250bbe7ae85cc1b34
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2010-07-15 13:12:25 +0000

    com_evecharshet - fix if clone is unknown

--- com_evecharsheet/site/views/character/tmpl/default_clone.php
+++ com_evecharsheet/site/views/character/tmpl/default_clone.php
@@ -9,5 +9,9 @@ defined('_JEXEC') or die();
 ?>
 <div class="evecharsheet-clone">
 	<h2><?php echo JText::_('Com_Evecharsheet_Clone'); ?></h2>
-	<?php echo JHTML::_('evelink.type', array($this->clone, 'clone')); ?>: <?php echo number_format($this->clone->cloneSkillPoints); ?> <?php echo JText::_('Com_Evecharsheet_Skill_Points'); ?>
+	<?php if ($this->clone): ?>
+		<?php echo JHTML::_('evelink.type', array($this->clone, 'clone')); ?>: <?php echo number_format($this->clone->cloneSkillPoints); ?> <?php echo JText::_('Com_Evecharsheet_Skill_Points'); ?>
+	<?php else: ?>
+		<?php echo JText::_('Com_Evecharsheet_Unknown_Clone')?>
+	<?php endif;?>
 </div>
