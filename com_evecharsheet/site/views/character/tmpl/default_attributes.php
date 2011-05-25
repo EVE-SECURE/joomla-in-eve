4d8409b6a28e28f06943c53a6564b2ba8a6d1545
kovalikp <kovalikp@1aeb54ac-7b53-0410-b98b-4bd7da8385b8>
2011-05-25 14:25:49 +0000

    removed learning skills

--- com_evecharsheet/site/views/character/tmpl/default_attributes.php
+++ com_evecharsheet/site/views/character/tmpl/default_attributes.php
@@ -13,8 +13,8 @@ defined('_JEXEC') or die();
 		<?php foreach ($this->attributes as $attribute): ?>
 			<tr>
 				<td><?php echo $attribute->attributeName; ?></td>
-				<td><?php echo ($attribute->value + $attribute->skillValue + $attribute->augmentatorValue) * $attribute->skillMultiplier; ?></td>
-				<td><?php echo $attribute->skillMultiplier; ?> x (<?php echo $attribute->value; ?> + <?php echo $attribute->skillValue; ?> + <?php echo $attribute->augmentatorValue; ?>)</td>
+				<td><?php echo $attribute->value + $attribute->augmentatorValue; ?></td>
+				<td>(<?php echo $attribute->value; ?> + <?php echo $attribute->augmentatorValue; ?>)</td>
 				<td><?php echo JHTML::_('evelink.type', array($attribute, 'augmentator')); ?></td>
 			</tr>
 		<?php endforeach; ?>
